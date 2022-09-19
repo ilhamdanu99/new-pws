70066ad3688368dfcb6679061ad8aaf920813f52
ilhamdanu99 <blacksentinel6@gmail.com>
2022-09-19 15:21:46 +0700

    haha

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('main')
+
+@section('container')
+<div class="container">
+  <h1>Halaman About</h1>
+</div>
+  <img src="https://indeksnews.com/wp-content/uploads/2022/08/Pengacara-dukun-1.jpg" class="rounded float-start" width="200" height="200" alt="...">
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Gus_Samsudin_Jadab.jpg/800px-Gus_Samsudin_Jadab.jpg" class="rounded float-end" width="200" height="200" alt="...">
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('main')
+
+@section('container')\
+<section id="" class="p-5 bg-light">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 mb-3">
+        <div class="card mb-3">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="https://cdn-image.hipwee.com/wp-content/uploads/2015/07/Mamang-Bakso.jpg" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Sekilas Tentang Beliau</h5>
+                <p class="card-text">Di depan kampus, ada mamang yang jualan tiap hari so kalo kelen laper habis dikasi deskjob, sprint deadline lagi yakan. Mamang ini jawaban semua dari kepenatan OTAK lu pade! Hehe Bakso is goooooooooood!!!</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+@endsection
